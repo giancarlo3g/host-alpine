fb96d93e1554ab73bc5fea5ab54b59ea189d682f
Giancarlo Garcia <giancarlo3g@gmail.com>
2025-01-15 17:30:58 +0000

    remove file not needed

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Building container image based on Dockerfile..."
-docker build --build-arg http_proxy=http://proxy.lbs.alcatel-lucent.com:8000 --build-arg https_proxy=http://proxy.lbs.alcatel-lucent.com:8000 -t host-alpine:latest .
-
